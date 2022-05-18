1713d1632a85ff28b1965aec33cd8fc40565d656
Ali El-Farou <aliel-farou@Alis-MBP.ht.home>
2022-05-18 00:04:21 -0400

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   
   resources :items
   resources :warehouses
-  resources :warehouse_items, only: [:new, :create, :destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   get "/", to: "warehouses#index", as: :root 
 end
